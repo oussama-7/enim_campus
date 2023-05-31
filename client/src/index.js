79da7beb39390a37aea7550ba38f405aa83b971e
soufiane-ba <111596987+soufiane-ba@users.noreply.github.com>
2023-05-31 23:42:24 +0000

    Update index.js
    deleting search-context from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-import { SearchContextProvider } from './context/SearchContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
@@ -13,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <SearchContextProvider>
         <StoreProvider>
           <HelmetProvider>
             <PayPalScriptProvider deferLoading={true}>
@@ -21,7 +19,6 @@ root.render(
             </PayPalScriptProvider>
           </HelmetProvider>
         </StoreProvider>
-      </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
